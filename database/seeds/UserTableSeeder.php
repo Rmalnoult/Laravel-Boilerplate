ce311d15ac6cebe8f3537644ebc792deee8bad9a
rmalnoult <romain.malnoult@gmail.com>
2017-09-04 16:38:27 +0200

    first commit

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+		$date = \Carbon\Carbon::now()->toDateTimeString();
+
+        DB::table('roles')->delete();
+        DB::table('roles')->insert(array(
+            array("id" => 1, "name" => 'admin', 'created_at' => $date, 'updated_at' => $date),
+        ));
+
+        DB::table('users')->delete();
+        DB::table('users')->insert(array(
+            array("id" => 1, "name" => 'romain', "password" => Hash::make('romain'), 'email' => 'romain@romain.com', 'created_at' => $date, 'updated_at' => $date),
+        ));
+
+        DB::table('role_user')->delete();
+        DB::table('role_user')->insert(array(
+            array("role_id" => 1, "user_id" => 1),
+        ));
+
+    }
+}
