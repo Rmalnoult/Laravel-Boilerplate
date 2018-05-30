0ca9a77dd7e712201a09474f7e457070de815ed4
rmalnoult <romain.malnoult@gmail.com>
2018-05-30 19:37:57 +0200

    changed style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
     @yield('scripts')
 </body>
 </html>
